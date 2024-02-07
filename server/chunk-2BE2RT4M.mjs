8f4401d3443acbafa2c668d0c064ad1ef6806200
kusumamadhavi1 <158534779+kusumamadhavi1@users.noreply.github.com>
2024-02-06 20:21:00 -0600

    Auto-generated commit

---
+++ server/chunk-2BE2RT4M.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as p,d as n,k as m,m as s,n as a,q as f,u as c,x as d}from"./chunk-6UJHWQXK.mjs";var l=(()=>{let o=class o{constructor(){this.title="DisplayData"}};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[n],decls:2,vars:0,template:function(e,h){e&1&&(p(0,`hellow
+`),i(1,"router-outlet"))},dependencies:[c]});let t=o;return t})();var u=[];var v={providers:[d(u),a()]};var x={providers:[f()]},g=m(v,x);var y=()=>s(l,g),B=y;export{B as a};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2BE2RT4M.mjs";import"./chunk-6UJHWQXK.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
